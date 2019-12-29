35f64b3eb6221be8134d061c51c39710a4266afb
tmrekk121 <tmrekk121@outlook.jp>
2019-12-30 01:24:10 +0900

    Show HTTP headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,25 +1,26 @@
 class ApplicationController < ActionController::API
   include ActionController::HttpAuthentication::Basic::ControllerMethods
-  before_action :basic_auth, except: [:signup, :index]
-  before_action :index_auth, only: [:index]
+  #before_action :basic_auth, except: [:signup, :index]
+  before_action :basic_auth, except: [:signup]
+  # before_action :index_auth, only: [:index]
 
   private
 
-  def index_auth
-    if request.headers['HTTP_AUTHORIZATION'].nil?
-      render status: 401, json: { 'message':'Authentication Faild' }
-    else
-      message = { 'message':'Authentication Faild' }.to_json
-      authenticate_or_request_with_http_basic(nil, message) do |user_id, password|
-        @user = User.find_by(user_id: user_id)
-        if @user.nil?
-          render status: 404, json: { 'message':'No User found' }
-        else
-          user_id == @user.user_id && password == @user.password
-        end
-      end
-    end
-  end
+  # def index_auth
+  #   if request.headers['HTTP_AUTHORIZATION'].nil?
+  #     render status: 401, json: { 'message':'Authentication Faild' }
+  #   else
+  #     message = { 'message':'Authentication Faild' }.to_json
+  #     authenticate_or_request_with_http_basic(nil, message) do |user_id, password|
+  #       @user = User.find_by(user_id: user_id)
+  #       if @user.nil?
+  #         render status: 404, json: { 'message':'No User found' }
+  #       else
+  #         user_id == @user.user_id && password == @user.password
+  #       end
+  #     end
+  #   end
+  # end
 
   def basic_auth
     if request.headers['HTTP_AUTHORIZATION'].nil?
