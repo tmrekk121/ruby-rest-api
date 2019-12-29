bb4ef49b6b780f3a487babf96d3887753bc92b42
tmrekk121 <tmrekk121@outlook.jp>
2019-12-30 01:05:16 +0900

    Show HTTP headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,7 @@ class ApplicationController < ActionController::API
   end
 
   def basic_auth
+    request.headers.sort.map { |k, v| logger.info "#{k}:#{v}" }
     message = { 'message':'Authentication Faild' }.to_json
     authenticate_or_request_with_http_basic(nil, message) do |user_id, password|
       if params[:user_id] != user_id && params[:user_id] != nil
@@ -38,3 +39,5 @@ end
 # curl -X PATCH -H 'Content-Type:application/json' -H 'Authorization:Basic dG1yZWtrMTIxOnBhc3N3b3Jk' -d '{ 'nickname': 'tmrekk121', 'comment': 'auth test' }' http://0.0.0.0:3000/users/tmrekk121
 # curl -X POST -H 'Content-Type:application/json' -d '{ 'user_id': 'testtest2', 'password': 'password' }' http://0.0.0.0:3000/signup
 # curl -X PATCH -H 'Content-Type:application/json' -H 'Authorization:Basic VGFyb1lhbWFkYTpQYVNTd2Q0VFk=' -d '{ 'nickname': 'たろー', 'comment': '僕は元気です' }'  https://ruby-rest-api.herokuapp.com/users/TaroYamada
+# curl -X POST -H 'Content-Type:application/json' -H 'Authorization:Basic dG1yZWtrMTIxOnBhc3N3b3Jk' https://ruby-rest-api.herokuapp.com/close
+# curl -X POST -H 'Content-Type:application/json' -d '{ "user_id": "tmrekk121", "password": "password" }' https://ruby-rest-api.herokuapp.com/signup
