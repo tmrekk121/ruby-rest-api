f82b538f164d368a6c56b9994cac82006d749ecb
tmrekk121 <tmrekk121@outlook.jp>
2019-12-30 01:21:00 +0900

    Show HTTP headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,13 +6,17 @@ class ApplicationController < ActionController::API
   private
 
   def index_auth
-    message = { 'message':'Authentication Faild' }.to_json
-    authenticate_or_request_with_http_basic(nil, message) do |user_id, password|
-      @user = User.find_by(user_id: user_id)
-      if @user.nil?
-        render status: 404, json: { 'message':'No User found' }
-      else
-        user_id == @user.user_id && password == @user.password
+    if request.headers['HTTP_AUTHORIZATION'].nil?
+      render status: 401, json: { 'message':'Authentication Faild' }
+    else
+      message = { 'message':'Authentication Faild' }.to_json
+      authenticate_or_request_with_http_basic(nil, message) do |user_id, password|
+        @user = User.find_by(user_id: user_id)
+        if @user.nil?
+          render status: 404, json: { 'message':'No User found' }
+        else
+          user_id == @user.user_id && password == @user.password
+        end
       end
     end
   end
